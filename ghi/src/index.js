cddd02dccc049a2dc0a906ee236273fda2718734
pizzawarrior <i.norstad@gmail.com>
2024-07-11 21:43:43 -0700

    added posthog for session recording

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -5,12 +5,23 @@ import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+const POSTHOG_API_KEY = process.env.REACT_APP_POSTHOG_API_KEY;
+
+posthog.init(POSTHOG_API_KEY, {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PostHogProvider client={posthog}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PostHogProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
