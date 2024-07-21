a555f6e7d9a465b038cbc80e10e178642f667d1f
pizzawarrior <i.norstad@gmail.com>
2024-07-20 18:21:39 -0700

    added another test for list_orders

--- api/tests/test_list_orders.py
+++ api/tests/test_list_orders.py
@@ -9,41 +9,39 @@ from queries.orders import OrderQueries
 client = TestClient(app)
 
 
-class MockOrderQueries:
-    def list_orders(self, dummy_order, dummy_order_2):
-        return {
-            dummy_order.dict(),
-            dummy_order_2.orders[0].dict()
-        }
-
-
-@pytest.mark.usefixtures("dummy_order", "dummy_order_2")
-class TestOrderQueries:
-    def list_mock_orders(self, dummy_order, dummy_order_2):
-        with patch.object(OrderQueries, "list_orders", new=MockOrderQueries().list_orders):
-            response = client.get("/orders")
-            assert response.status_code == 200
-            assert response.json() == {
-                "orders": [
-                    dummy_order.dict(),
-                    dummy_order_2.orders[0].dict()
-                ]
-            }
-
-
 class EmptyOrderQueries:
     def list_orders(self):
         return []
 
 
-def test_list_empty_orders():
-    # Arrange
-    app.dependency_overrides[OrderQueries] = EmptyOrderQueries
+class TestUser:
+    def test_list_empty_orders(self):
+        with patch.object(OrderQueries, "list_orders", new=EmptyOrderQueries().list_orders):
+            response = client.get("/orders")
 
-    response = client.get("/orders")
+            expected = {"orders": []}
 
-    # Assert
-    assert response.status_code == 200
-    assert response.json() == {"orders": []}
+            # Assert
+            assert response.status_code == 200
+            assert response.json() == expected
 
-    app.dependency_overrides = {}
+    def test_list_orders_with_data(self, dummy_order):
+        class MockOrderQueriesWithData:
+            def list_orders(self):
+                return [
+                    dummy_order.dict(),
+                    dummy_order.dict()
+                ]
+
+        with patch.object(OrderQueries, "list_orders", new=MockOrderQueriesWithData().list_orders):
+            response = client.get("/orders")
+
+            expected = {
+                "orders": [
+                    dummy_order.dict(),
+                    dummy_order.dict()
+                ]
+            }
+
+            assert response.status_code == 200
+            assert response.json() == expected
